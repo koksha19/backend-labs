abaf9c1fcd53b89de77ef081e0eb62647cc99e65
Lev <lev.bereza@gmail.com>
2025-11-10 22:11:15 +0200

    feat(lab-3): add global validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
+import { UserService } from './users/users.service';
+import { UserController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { RecordsModule } from './records/records.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { RecordController } from "./records/records.controller";
+import { CategoryController } from "./categories/categories.controller";
+import { RecordService } from "./records/records.service";
+import { CategoryService } from "./categories/categories.service";
 
 @Module({
   imports: [UsersModule, CategoriesModule, RecordsModule, PrismaModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, PrismaService],
+  controllers: [AppController, UserController, CategoryController, RecordController],
+  providers: [AppService, UserService, PrismaService, CategoryService, RecordService],
 })
 export class AppModule {}
