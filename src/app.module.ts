152ba2a052fdb6a56f54045ad0709aae49869883
Lev <lev.bereza@gmail.com>
2025-11-15 16:36:45 +0200

    feat(lab-4): fix modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './users/users.service';
-import { UserController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { RecordsModule } from './records/records.module';
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { RecordController } from "./records/records.controller";
-import { CategoryController } from "./categories/categories.controller";
-import { RecordService } from "./records/records.service";
-import { CategoryService } from "./categories/categories.service";
-import { AuthController } from "./auth/auth.controller";
-import { AuthService } from "./auth/auth.service";
 
+import { AuthModule } from "./auth/auth.module";
 @Module({
-  imports: [UsersModule, CategoriesModule, RecordsModule, PrismaModule],
-  controllers: [AppController, UserController, CategoryController, RecordController, AuthController],
-  providers: [AppService, UserService, PrismaService, CategoryService, RecordService, AuthService],
+  imports: [
+    UsersModule,
+    CategoriesModule,
+    RecordsModule,
+    PrismaModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from './jwt.strategy';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [
-    PassportModule,
     JwtModule.register({
-      secret: 'supersecretkey',
+      secret: 'supersecret',
       signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, PrismaService],
+  providers: [AuthService, PrismaService],
   exports: [AuthService],
 })
 export class AuthModule {}
